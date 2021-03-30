27b0401e2c48a3485e9f50c7ffbc76b141a9908d
chutima <chuthima0669@gmail.com>
2021-03-30 10:34:13 +0700

    b1

--- src/LAB9/MIT_Company_App.java
+++ src/LAB9/MIT_Company_App.java
@@ -4,11 +4,12 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class MIT_Company_App {
-    public static void main(String[] args){
-        //step 1;
+
+    public static void main(String[] args) {
+        //step 1:load driver
         try {
             Class.forName("org.sqlite.JDBC");
-        }catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -21,18 +22,16 @@ public class MIT_Company_App {
                 System.out.println("Could not connect to database.");
             else
                 System.out.println("Database is connected.");
-
-            //step 3:create statements
+            //step 3:create statement
             Statement stmt = conn.createStatement();
             String sql = "select * from Employee";
 
-            //step4: get data from database
+            //step 4:get data from database
             ResultSet rs = stmt.executeQuery(sql);
-
             if (rs == null)
-                System.out.println("Could not found any data in database");
+                System.out.println("Could not found any data in database.");
             else {
-                ArrayList<Employee> myEmp = new ArrayList<Employee>();
+                ArrayList<Employee> myemp = new ArrayList<Employee>();
                 while (rs.next()){
 //                    System.out.println(rs.getString(1));
 //                    System.out.println(rs.getString(2));
@@ -44,27 +43,31 @@ public class MIT_Company_App {
                             rs.getString(3),
                             rs.getString(4),
                             rs.getDouble(5));
-                    myEmp.add(emp);
+                    myemp.add(emp);
+
                 }//while
-                DisplayObject(myEmp);
+                DisplayObject(myemp);
+
             }
-            //step 5 close Connection
+            //step 5:close connection
             rs.close();
             stmt.close();
             conn.close();
-
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
-    }
 
-    private static void DisplayObject(ArrayList<Employee> myEmp) {
+    }//main
+
+    private static void DisplayObject(ArrayList<Employee> myemp) {
         System.out.println("Data from database: ");
-        for (Employee e:myEmp){
+        for (Employee e:myemp) {
             System.out.println(e.toString());
+
         }
     }
-}
+
+}//class
